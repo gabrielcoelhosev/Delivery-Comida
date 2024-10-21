b1dd857fae94972e019210b3487a708291a7620e
gabrielcoelhosev <gabrielcoelhoseverino6915@gmail.com>
2024-10-20 21:00:23 -0300

    files add

---
+++ android/app/src/main/kotlin/com/example/delivery_comida/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.delivery_comida
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/main.dart
@@ -0,0 +1,6 @@
+import 'package:delivery_comida/core/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
